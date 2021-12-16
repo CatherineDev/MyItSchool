2e6e52962a77c172881bcd55805944bc6401baf1
Catherine <miss.ignatovec@mail.ru>
2021-12-16 18:32:46 +0300

    New HW

---
+++ HW_Cycles/scripts.js
@@ -0,0 +1,80 @@
+// 1 задание
+
+//for(let i = 1; i<=50; i++ ) console.log (i);
+//for(let i = 35; i>=8; i-- ) console.log (i);
+
+// 2 задание
+
+//for(let i = 89; i>=11; i-- ){
+//    document.write (i + "<br>");
+//}
+
+//3 задание
+
+//let a = 100;
+//let sum = 0;
+//for (let i = 1; i<=a; i++){
+    //sum+=i
+//}
+//console.log (sum);
+
+
+
+//4 задание
+
+//let sum = 0;
+//let b = 5;
+
+//for (let i = 1; i<=b; i++) {
+        //sum=0;
+    //for(let a = 1; a<=i; a++){
+       //sum += a;
+    //}
+    //console.log (sum);
+//}
+
+//5 задание
+
+//i=8
+//while (i<=56){
+    //if (i % 2 == 0){
+        //console.log (i);
+    //}
+    //i++;
+//}
+
+//for (i=8; i<56; i++) {
+    //if (i % 2 == 0){
+        //console.log (i);
+    //}
+//}
+
+//6 задание
+
+//let sum;
+//for (let a=2;a<=10; a++)
+    //for(let i = 1; i<=10; i++){
+        //sum = a*i;
+        //console.log (a + "*" + i + "=" + sum);
+//}
+
+// 7 задание 
+
+//let n=1000
+//let num=0;
+
+//while (n>50) {
+    //n = n/2;
+    //console.log (n);
+    //num = num+1
+//}
+//console.log (num + " итераций");
+
+// 9 задание
+
+//let str = "4 98 4 6 1 32 4 65 4 3 5 7 89 7 10 1 36 8 57"
+//let str2 = str.split(" ");
+//console.log (str2);
+
+//for (let i = 0; i < str2.length; i++) {
+//}
